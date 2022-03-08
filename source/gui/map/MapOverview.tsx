5a08446dafe2282e1e64aa6a909ee0ae6b5cdc8a
S. Jansen <sa.jansen1@gmail.com>
2022-03-08 16:42:52 +0200

    feat(map): Only load locations when map is visible.

--- source/gui/map/MapOverview.tsx
+++ source/gui/map/MapOverview.tsx
@@ -1,22 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { useRecoilValue } from "recoil";
 import { selectedDateOrdersState } from "../../logic/recoil";
 import { Location, Locations } from "../../logic/location/Locations";
 import Map from "./Map";
+import { useFocusEffect } from "@react-navigation/native";
 
 interface Props {
 
 }
 
 const MapOverview: React.FC<Props> = () => {
+  const isMounted = useRef(false);
+  const isDirty = useRef(false);
   const orders = useRecoilValue(selectedDateOrdersState);
   const [locations, setLocations] = useState<Location[]>([]);
 
+  useFocusEffect(React.useCallback(() => {
+    isMounted.current = true;
+
+    if (isDirty.current) {
+      loadLocations();
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, []));
+
   useEffect(() => {
+    if (!isMounted.current) {
+      isDirty.current = true;
+      return;
+    }
+    loadLocations();
+  }, [orders]);
+
+  const loadLocations = () => {
     Locations.locationsForOrders(orders)
       .then((result) => setLocations(result));
-  }, [orders]);
+  };
 
   return <View style={styles.container}>
     <Map locations={locations} />
