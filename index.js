03780da37ad2c4f9c622f7ce59029258f4a8cacc
S. Jansen <sa.jansen1@gmail.com>
2022-05-03 09:32:30 +0200

    fix(location): Defined console.dir function in order to try to fix random error rollbar #3 TypeError: console.dir is not a function

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './source/App';
 import {name as appName} from './app.json';
 
+// Dirty fix for react-native-cache (https://github.com/timfpark/react-native-cache/issues/42#issuecomment-1049938333)
+console.dir = () => {};
+
 AppRegistry.registerComponent(appName, () => App);
