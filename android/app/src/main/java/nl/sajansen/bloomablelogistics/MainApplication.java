aa15a7fa1482c02d7693ed4e46f7d085115d42a2
S. Jansen <sa.jansen1@gmail.com>
2022-10-20 21:28:47 +0200

    feat: Upgraded to react native 0.70.3 (build is working, code not yet)

--- android/app/src/main/java/nl/sajansen/bloomablelogistics/MainApplication.java
+++ android/app/src/main/java/nl/sajansen/bloomablelogistics/MainApplication.java
@@ -12,7 +12,9 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import nl.sajansen.bloomablelogistics.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.rollbar.RollbarReactNative;
@@ -41,14 +43,23 @@ public class MainApplication extends Application implements ReactApplication {
         }
       };
 
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     RollbarReactNative.init(this, "de2d7d5406834a59814740bac636faff", "production");
