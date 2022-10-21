a7559baaf16b36690a353aa66ff01af6ec6dc0bb
S. Jansen <sa.jansen1@gmail.com>
2022-10-21 20:21:10 +0200

    feat: Upgraded other packages.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    ["@babel/plugin-transform-flow-strip-types", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    "react-native-reanimated/plugin",
+  ],
 };
