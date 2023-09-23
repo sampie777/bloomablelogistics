04e24406430c5459c3b9ee7c373e4beecacad689
S. Jansen <sa.jansen1@gmail.com>
2023-09-23 16:07:50 +0200

    styling

--- source/logic/demoData/mocks.ts
+++ source/logic/demoData/mocks.ts
@@ -67,14 +67,17 @@ export namespace Mocks {
     fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
       if (input === "https://dashboard.bloomable.com/api/me") {
         return delayedPromiseWithValue({ ...defaultResponse(), json: () => Promise.resolve(demoResponseMe) }, 500);
+
       } else if (input === "https://dashboard.bloomable.com/api/orders?page=1&s=created_at&d=desc") {
         return delayedPromiseWithValue({ ...defaultResponse(), json: () => Promise.resolve(demoResponseOrders) }, 500);
+
       } else if (typeof (input) === "string" && RegExp("https://dashboard.bloomable.com/api/product-variants/\\d+$", "gi").test(input)) {
         const id = +Array.from(input.matchAll(RegExp("https://dashboard.bloomable.com/api/product-variants/(\\d+)$", "gi")))[0][1];
         return delayedPromiseWithValue({
           ...defaultResponse(),
           json: () => Promise.resolve(demoResponseProduct[id]),
         }, 500);
+
       } else if (typeof (input) === "string" && RegExp("https://dashboard.bloomable.com/api/orders/\\d+$", "gi").test(input)) {
         const id = Array.from(input.matchAll(RegExp("https://dashboard.bloomable.com/api/orders/(\\d+)$", "gi")))[0][1];
         const order = demoResponseOrders.data.find(it => it.id === id);
@@ -82,8 +85,10 @@ export namespace Mocks {
           ...defaultResponse(),
           json: () => Promise.resolve({ data: order }),
         }, 500);
+
       } else if (typeof (input) === "string" && RegExp("https://dashboard.bloomable.com/api/orders/\\d+/(accept|reject|fulfill|deliver)$", "gi").test(input)) {
         return delayedPromiseWithValue(defaultResponse(), 500);
+        
       } else if (typeof (input) === "string" && RegExp("https://maps.google.com/.*$").test(input)) {
         return originalFetch(input, init);
       }
